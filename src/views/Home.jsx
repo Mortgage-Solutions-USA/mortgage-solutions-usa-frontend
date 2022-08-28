78801ad51357d7ac6da01af3b571e0058b922b2b
eludden35 <elonzo.ludden@gmail.com>
2022-08-28 15:20:05 -0500

    adjustments for mobile

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -335,7 +335,7 @@ const Home = () => {
                   <Col className="text-center">
                     <h2>
                       YOUR{" "}
-                      <span className="text-success" style={{ width: "100%" }}>
+                      <span className="text-success">
                         <strong>TRUSTED</strong>
                       </span>{" "}
                       MORTGAGE EXPERTS
