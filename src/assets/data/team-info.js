2f4b4cc1b3f8118a0893e60ab4aa17213fff47b4
eludden35 <elonzo.ludden@gmail.com>
2023-03-08 11:15:29 -0600

    alter order of team and alter chads url

--- src/assets/data/team-info.js
+++ src/assets/data/team-info.js
@@ -11,6 +11,16 @@ const teamInfo = [
   },
   {
     id: 2,
+    name: "Chad Cody",
+    firstName: "Chad",
+    title: "Mortgage Loan Originator",
+    license: "NMLS # 1656573",
+    url: "https://cody.zipforhome.com/",
+    img: "../assets/img/chad-headshot.png",
+    desc: "Buying a home or investment property is an important decision that can be both exciting and daunting. That's why I'm here to help. With my many years of experience in originating residential mortgage loans, I have the knowledge and expertise to ensure you get the best loan solution for your situation. As a broker, I have access to multiple wholesale lenders and hundreds of loan programs, including construction loans with as little as 5% down. But I'm more than just a professional - I'm a father of an amazing son and a devoted husband to my wife, who is also in the real estate business. We work together to help our clients through a smooth transaction, while leveraging our time to spend with our son. If you're ready to take the next step, you can get started quickly by completing an application through our secure website or by giving me a call. I'm available to answer any questions, 7 DAYS A WEEK. Let me help you realize your dream of owning your own home or investing in a property to add to your portfolio. I look forward to helping you on your journey to owning your dream property!",
+  },
+  {
+    id: 3,
     name: "Katelynn Frank",
     firstName: "Katelynn",
     title: "Mortgage Load Originator",
@@ -20,7 +30,7 @@ const teamInfo = [
     desc: "Hello, I am Katelynn Frank a born and raised resident of Mid Missouri. My husband and I moved our family to Eldon, Missouri in 2021 and we love our community! I have been in the mortgage industry since January of 2021 and am happy to say that I have finally found a career where my heart belongs. After years of a career in the medical field, it in ingrained in me to serve, help, and solve for people and families. My promise to you as my client is to listen, communicate, and work to find the best loan product for your individual needs, all while making the process of purchasing your dream home or refinancing your forever home as seamless and stress free as possible! I truly look forward to working with you and your family to make the dream of home ownership a reality. Let's get started! ",
   },
   {
-    id: 3,
+    id: 4,
     name: "Ashley Taillon",
     firstName: "Ashley",
     title: "Director of Marketing / Mortgage Loan Originator",
@@ -28,16 +38,6 @@ const teamInfo = [
     img: "../assets/img/ashley-headshot.png",
     url: "https://ashley.zipforhome.com/",
     desc: "My name is Ashley Taillon! I have been in the mortgage industry for 3 years now! I started as a loan officer assistant, and then moved to processing making me involved in all of the back end work that goes on with your mortgage! I am now a licensed mortgage loan originator!I am originally from upstate New York. I moved to Columbia, Missouri in September of 2018 and love it! Missouri is my new home. I am a cat mom to two big furballs named Ozzy and Lola. I like camping in tents, hiking, swimming, bike riding, singing, doing yoga, and of course, playing with my kitty cats! I am looking forward to helping you reach your dreams revolving around your home purchase or refinance! You deserve to feel at home and the help of an honest lender. My vow to you is just that.",
-  },
-  {
-    id: 4,
-    name: "Chad Cody",
-    firstName: "Chad",
-    title: "Mortgage Loan Originator",
-    license: "NMLS # 1656573",
-    url: "https://chad.zipforhome.com/",
-    img: "../assets/img/chad-headshot.png",
-    desc: "Buying a home or investment property is an important decision that can be both exciting and daunting. That's why I'm here to help. With my many years of experience in originating residential mortgage loans, I have the knowledge and expertise to ensure you get the best loan solution for your situation. As a broker, I have access to multiple wholesale lenders and hundreds of loan programs, including construction loans with as little as 5% down. But I'm more than just a professional - I'm a father of an amazing son and a devoted husband to my wife, who is also in the real estate business. We work together to help our clients through a smooth transaction, while leveraging our time to spend with our son. If you're ready to take the next step, you can get started quickly by completing an application through our secure website or by giving me a call. I'm available to answer any questions, 7 DAYS A WEEK. Let me help you realize your dream of owning your own home or investing in a property to add to your portfolio. I look forward to helping you on your journey to owning your dream property!",
   },
 ];
 
